93200e7d73c030c2407e81aeddf7469461b8ffa7
Zahidul Islam <george04023@gmail.com>
2016-05-03 14:08:43 +0600

    This is the first commit

--- app/src/main/java/com/example/geo/gittestapplication/MainActivity.java
+++ app/src/main/java/com/example/geo/gittestapplication/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.geo.gittestapplication;
 
+import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tvText = (TextView)findViewById(R.id.tvText);
+        tvText.setText("This is a textview");
     }
 }
